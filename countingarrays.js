6f71ff559b0dc24201922c4543eb15ed6f28edd7
sofia amoroso <sofi79a0@stud.kea.dk>
2023-02-10 14:48:35 +0100

    start

---
+++ countingarrays.js
@@ -0,0 +1,17 @@
+"use strict"
+console.log("hello world");
+
+// use:
+// variable counter
+// variable array
+// function with init-loop pattern
+
+
+
+
+// make a program that counts with a timed delay
+
+// mdify the program to insert the counter-value in the baginning of the array
+
+// modify the program again to make sure tha array doesn't get onger than nine item
+
